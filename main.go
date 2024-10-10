4c480f83d946c9de7e1828755b953a3b8f029176
eddieljs <2574350328.com>
2024-10-10 16:06:25 +0800

    后端更新2.2

--- main.go
+++ main.go
@@ -1,44 +1,26 @@
 package main
 
 import (
-	"io"
+	"fmt"
+	"log"
 	"net/http"
 	"os"
-	"untitled/routers"
-
-	"github.com/gin-gonic/gin"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Print("收到访问请求！")
+	target := "欢迎使用微信云托管"
+	fmt.Fprintf(w, "Hello, %s!\n", target)
+}
+
 func main() {
-	// 禁用控制台颜色，将日志写入文件时不需要控制台颜色。
-	gin.DisableConsoleColor()
+	log.Print("微信云托管服务启动成功")
 
-	// 记录到文件。
-	f, _ := os.Create("gin.log")
-	// gin.DefaultWriter = io.MultiWriter(f)
+	http.HandleFunc("/", handler)
 
-	// 如果需要同时将日志写入文件和控制台，请使用以下代码。
-	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
-
-	r := gin.Default()
-
-	// CORS中间件
-	cors := func(c *gin.Context) {
-		// 允许特定的域进行跨域请求
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		// 允许特定的请求方法
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		// 允许特定的请求头
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		// 允许携带身份凭证（如Cookie）
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		if c.Request.Method == http.MethodOptions {
-			c.AbortWithStatus(http.StatusOK)
-		} else {
-			// 继续处理请求
-			c.Next()
-		}
-
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "80"
 	}
 	print()
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
